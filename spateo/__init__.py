8c3a1555e66269fe269fb95567582ebbb5c98a63
qiuping1 <qiuping1@genomics.cn>
2021-03-05 16:57:45 +0800

    init the frame

---
+++ spateo/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+@author: Ping Qiu  qiuping1@genomics.cn
+@last modified by: Ping Qiu
+@file:__init__.py.py
+@time:2021/03/05
+"""
+
+if __name__ == '__main__':
+    pass
