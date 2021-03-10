74fcbfc575a2fb65d54bf874b0eda3aed30c257f
qiuping1 <qiuping1@genomics.cn>
2021-03-10 17:12:32 +0800

    add the functions of preprocess. `filter.py` and `normalize.py`

--- spateo/preprocess/normalize.py
+++ spateo/preprocess/normalize.py
@@ -6,3 +6,25 @@
 @file:normalize.py
 @time:2021/03/05
 """
+from scipy.sparse import issparse
+import numpy as np
+
+
+def normalize(andata, target_sum=1, inplace=True):
+    exp_matrix = andata.X
+    if issparse(exp_matrix):
+        exp_matrix = exp_matrix.toarray()
+    nor_exp_matrix = exp_matrix * target_sum / exp_matrix.sum(axis=1)[:, np.newaxis]
+    if inplace:
+        andata.X = nor_exp_matrix
+    return nor_exp_matrix if not inplace else None
+
+
+def log1p(andata, inplace=True):
+    exp_matrix = andata.X.copy()
+    if issparse(exp_matrix):
+        exp_matrix = exp_matrix.toarray()
+    log_x = np.log1p(exp_matrix, out=exp_matrix)
+    if inplace:
+        andata.X = log_x
+    return log_x if not inplace else None
