093131c568ee8f5b54a72df6053be8ef5a7523d0
qiuping1 <qiuping1@genomics.cn>
2021-03-17 15:22:32 +0800

    add quantile_norm func od `normalize.py` and refactor the code.

--- stereo/preprocess/normalize.py
+++ stereo/preprocess/normalize.py
@@ -7,44 +7,104 @@
 @time:2021/03/05
 
 change log:
-    add basic functions of normalization. by Ping Qiu.
+    add basic functions of normalization. by Ping Qiu. 2021/03/05
+    Refactor the code and add the quantile_norm function. by Ping Qiu. 2021/03/17
 """
 from scipy.sparse import issparse
 import numpy as np
 from typing import Union
 from anndata import AnnData
 from pandas import DataFrame
+from scipy import stats
+from ..log_manager import logger
 
 
-def normalize(data: Union[AnnData, DataFrame], target_sum=1, inplace=True):
+class Normalizer(object):
     """
-    total count normalize the data to `target_sum` reads per cell, so that counts become comparable among cells.
-    :param data: AnnData object or Dataframe, which row is cells and column is genes.
-    :param target_sum: the number of reads per cell after normalization.
-    :param inplace: whether inplace the original anndata or return a new anndata. only use for AnnData format.
-    :return:
+    Normalizer of stereo.
     """
-    exp_matrix = data.X if isinstance(data, AnnData) else data.values
-    if issparse(exp_matrix):
-        exp_matrix = exp_matrix.toarray()
-    nor_exp_matrix = exp_matrix * target_sum / exp_matrix.sum(axis=1)[:, np.newaxis]
-    if inplace and isinstance(data, AnnData):
-        data.X = nor_exp_matrix
-        return None
-    return nor_exp_matrix
+    def __init__(self, data: Union[AnnData, DataFrame], method='normalize_total', inplace=True, target_sum=1):
+        """
 
+        :param data:
+        :param method:
+        :param inplace:
+        :param target_sum:
+        """
+        self.data = data
+        self.method = method
+        self.inplace = inplace
+        self.target_num = target_sum
+        self.exp_matrix = self.data.values if isinstance(self.data, DataFrame) else self.data.X.copy()
+        self.check_param()
 
-def log1p(andata, inplace=True):
-    """
-    Logarithmize the data. log(1 + x)
-    :param andata: AnnData object.
-    :param inplace: whether inplace the original adata or return a new anndata.
-    :return:
-    """
-    exp_matrix = andata.X.copy()
-    if issparse(exp_matrix):
-        exp_matrix = exp_matrix.toarray()
-    log_x = np.log1p(exp_matrix, out=exp_matrix)
-    if inplace:
-        andata.X = log_x
-    return log_x if not inplace else None
+    def check_param(self):
+        """
+        Check whether the parameters meet the requirements.
+        :return:
+        """
+        if self.method not in ['normalize_tital', 'quantile']:
+            logger.error(f'{self.method} is out of range, please check.')
+            raise ValueError(f'{self.method} is out of range, please check.')
+
+    def sparse2array(self):
+        """
+        transform `self.exp_matrix` to np.array if it is sparseMatrix.`
+        :return:
+        """
+        if issparse(self.exp_matrix):
+            self.exp_matrix = self.exp_matrix.toarray()
+        return self.exp_matrix
+
+    def fit(self):
+        """
+        compute the scale value of self.exp_matrix.
+        :return:
+        """
+        nor_res = None
+        self.sparse2array()  # TODO: add  normalize of sparseMatrix
+        if self.method == 'normalize_total':
+            nor_res = self.normalize_total(self.exp_matrix, self.target_num)
+        elif self.method == 'quantile':
+            nor_res = self.quantile_norm(self.exp_matrix)
+        else:
+            pass
+        if nor_res and self.inplace and isinstance(self.data, AnnData):
+            self.data.X = nor_res
+        return nor_res
+
+    @staticmethod
+    def normalize_total(x, target_sum):
+        """
+        total count normalize the data to `target_sum` reads per cell, so that counts become comparable among cells.
+        :param x: 2D array, shape (M, N), which row is cells and column is genes.
+        :param target_sum: the number of reads per cell after normalization.
+        :return: the normalized data.
+        """
+        nor_x = x * target_sum / x.sum(axis=1)[:, np.newaxis]
+        return nor_x
+
+    @staticmethod
+    def quantile_norm(x):
+        """
+        Normalize the columns of X to each have the same distribution.
+        Given an expression matrix  of M genes by N samples, quantile normalization ensures all samples have the same
+        spread of data (by construction).
+        :param x: 2D array of float, shape (M, N)
+        :return: The normalized data.
+        """
+        quantiles = np.mean(np.sort(x, axis=0), axis=1)
+        ranks = np.apply_along_axis(stats.rankdata, 0, x)
+        rank_indices = ranks.astype(int) - 1
+        xn = quantiles[rank_indices]
+        return xn
+
+    @staticmethod
+    def log1p(x):
+        """
+        Logarithmize the data. log(1 + x)
+        :param x: 2D array, shape (M, N).
+        :return:
+        """
+        log_x = np.log1p(x, out=x)
+        return log_x
