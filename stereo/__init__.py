37242d0ff74f4492b5bc0a9ef28284fa63f97e2b
molindoudou <qdh504f@126.com>
2021-05-27 09:26:50 +0800

    update api index

--- stereo/__init__.py
+++ stereo/__init__.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 # coding: utf-8
-"""
-
-@author: Ping Qiu  qiuping1@genomics.cn
-@last modified by: Ping Qiu
-@file:__init__.py.py
-@time:2021/03/05
-"""
 from . import plots as plt
 import sys
 from .config import StereoConfig
