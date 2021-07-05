0f8b9ac46b00606b676343e70687525b03b72ae0
qiupinghust <qiupinghust@163.com>
2021-07-05 17:35:03 +0800

    add segmentation funcs

---
+++ stereo/image/segmentation/segment.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import seg_utils.cell_seg_pipeline as pipeline
+
+
+def cell_seg(img_path, out_path, flag, depp_cro_size=20000, overlap=100, gpu=None):
+    """
+    cell segmentation.
+
+    :param img_path: image path
+    :param out_path: the ouput path of mask result
+    :param flag: watershed
+    :param depp_cro_size: deep crop size
+    :param overlap: the size of overlap
+    :param gpu: the id of gpu, if None,use the cpu to predict.
+    :return:
+    """
+    if gpu is not None:
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+    cell_seg_pipeline = pipeline.CellSegPipe(img_path, out_path, flag, depp_cro_size, overlap)
+    cell_seg_pipeline.run()
