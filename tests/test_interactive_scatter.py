d9f8dfe99d2af023bb2a2cb6d9a636d6a4b59eb3
molindoudou <qdh504f@126.com>
2021-08-11 14:04:06 +0800

    update requirements.txt

--- tests/test_interactive_scatter.py
+++ tests/test_interactive_scatter.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import panel as pn
 from stereo.io.reader import read_stereo
-from stereo.plots.interactive_scatter import InteractiveScatter
+# import pandas as pd
+# import panel as pn
+# from stereo.plots.interactive_scatter import InteractiveScatter
 
 gem = 'D:\\projects\\data\\st_demo\\DP8400013846TR_F5.gem'
 data = read_stereo(gem, 'bins', 20)
