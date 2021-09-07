269ae244c7571b4a880e3c4a07c8636330ede5e2
wuyiran55 <wuyiran55@outlook.com>
2021-09-07 10:57:12 +0800

    change neighbor func.

--- stereo/algorithm/neighbors.py
+++ stereo/algorithm/neighbors.py
@@ -1,113 +1,244 @@
 #!/usr/bin/env python3
 # coding: utf-8
 """
-@file: neighbors.py
-@description: 
-@author: Yiran Wu
-@email: wuyiran@genomics.cn
+@author: Yiran Wu  wuyiran@genomics.cn
 @last modified by: Yiran Wu
-
-change log:
-    2021/9/3 create file.
+@file:neighbors.py
+@time:2021/09/01
 """
-
-from ..utils.data_helper import select_group
-from ..core.tool_base import ToolBase
-from ..log_manager import logger
-from tqdm import tqdm
-from typing import Union, Sequence
+from scipy.sparse import issparse,coo_matrix,csr_matrix
+from sklearn.neighbors import NearestNeighbors
+import igraph as ig
 import numpy as np
-from ..plots.marker_genes import plot_marker_genes_text, plot_marker_genes_heatmap
-from ..algorithm.neighbors import Neighbors
+
 from sklearn.metrics import pairwise_distances
-from typing import Optional
-import pandas as pd
+from ..log_manager import logger
+from sklearn.utils import check_random_state
+from numpy import random
 
-class Neighbors(ToolBase):
-    """
-    a tool of finding maker gene
-    for each group, find statistical test different genes between one group and the rest groups using t-test or wilcoxon_test
-    :param data: expression matrix, StereoExpData object
-    :param groups: group information matrix, at least two columns, treat first column as sample name, and the second as
-    group name e.g pd.DataFrame({'bin_cell': ['cell_1', 'cell_2'], 'cluster': ['1', '2']})
-    :param case_groups: default all clusters
-    :param control_groups: rest of groups
-    :param method: t-test or wilcoxon_test
-    :param corr_method: correlation method
-    Examples
-    --------
-    >>> from stereo.tools.find_markers import FindMarker
-    >>> fm = FindMarker()
-    """
+class Neighbors(object):
+    def __init__(self, x, n_neighbors=10, n_pcs=40,metric='euclidean', method='umap',knn=True):
+        self.x = x
+        self.n_neighbors = n_neighbors
+        self.n_pcs = n_pcs
+        self.metric = metric
+        self.method = method
+        self.knn = knn
 
-    def __init__(
+    def check_setting(self):
+        if self.method == 'umap' and not self.knn :
+            logger.error(f'method=umap only with knn=True')
+        if self.method not in {'umap', 'gauss'} :
+            logger.error(f'method=umap/gauss')
+
+    def choose_x(self):
+        self.x = self.x.iloc[:, :self.n_pcs]
+        return self.x
+
+    def get_indices_distances_from_sparse_matrix(self,D, n_neighbors: int):
+        indices = np.zeros((D.shape[0], n_neighbors), dtype=int)
+        distances = np.zeros((D.shape[0], n_neighbors), dtype=D.dtype)
+        n_neighbors_m1 = n_neighbors - 1
+        for i in range(indices.shape[0]):
+            neighbors = D[i].nonzero()  # 'true' and 'spurious' zeros
+            indices[i, 0] = i
+            distances[i, 0] = 0
+            # account for the fact that there might be more than n_neighbors
+            # due to an approximate search
+            # [the point itself was not detected as its own neighbor during the search]
+            if len(neighbors[1]) > n_neighbors_m1:
+                sorted_indices = np.argsort(D[i][neighbors].A1)[:n_neighbors_m1]
+                indices[i, 1:] = neighbors[1][sorted_indices]
+                distances[i, 1:] = D[i][
+                    neighbors[0][sorted_indices], neighbors[1][sorted_indices]
+                ]
+            else:
+                indices[i, 1:] = neighbors[1]
+                distances[i, 1:] = D[i][neighbors]
+        return indices, distances
+
+    def get_indices_distances_from_dense_matrix(self,D, n_neighbors: int):
+        sample_range = np.arange(D.shape[0])[:, None]
+        indices = np.argpartition(D, n_neighbors - 1, axis=1)[:, :n_neighbors]
+        indices = indices[sample_range, np.argsort(D[sample_range, indices])]
+        distances = D[sample_range, indices]
+        return indices, distances
+
+    def get_parse_distances_numpy(self,indices, distances, n_obs,):
+        n_nonzero = n_obs * self.n_neighbors
+        indptr = np.arange(0, n_nonzero + 1, self.n_neighbors)
+        D = csr_matrix(
+            (
+                distances.copy().ravel(),  # copy the data, otherwise strange behavior here
+                indices.copy().ravel(),
+                indptr,
+            ),
+            shape=(n_obs, n_obs),
+        )
+        D.eliminate_zeros()
+        return D
+
+    def get_parse_distances_umap(self, nn_idx, nn_dist):
+        n_obs = self.x.shape[0]
+        rows = np.zeros((n_obs * self.n_neighbors), dtype=np.int64)
+        cols = np.zeros((n_obs * self.n_neighbors), dtype=np.int64)
+        vals = np.zeros((n_obs * self.n_neighbors), dtype=np.float64)
+
+        for i in range(nn_idx.shape[0]):
+            for j in range(self.n_neighbors):
+                if nn_idx[i, j] == -1:
+                    continue  # We didn't get the full knn for i
+                if nn_idx[i, j] == i:
+                    val = 0.0
+                else:
+                    val = nn_dist[i, j]
+
+                rows[i * self.n_neighbors + j] = i
+                cols[i * self.n_neighbors + j] = nn_idx[i, j]
+                vals[i * self.n_neighbors + j] = val
+
+        distances = coo_matrix((vals, (rows, cols)), shape=(n_obs, n_obs))
+        distances.eliminate_zeros()
+        return distances.tocsr()
+
+    def compute_neighbors_umap(
             self,
-            method: str = 'umap',
-            pca_x: Optional[pd.DataFrame] = None,
-            n_neighbors: Optional[int] = 30,
-            n_pcs: Optional[int] = 40,
-            metric: Optional[str] = 'euclidean',
-            knn: Optional[bool] = True,
-            find_neighbors: bool = False,
-            clustering: bool = True,
-            pca_pcs: Optional[int] = 50,
+            X,
+            #random_state: AnyRandom = None,
+            angular: bool = False,
+            verbose: bool = False,
     ):
-        super(Neighbors, self).__init__(pca_x=pca_x)
-        self.n_pcs = n_pcs,
-        self.metric = metric,
-        self.method = method,
-        self.knn = knn,
-        self.find_neighbors = find_neighbors,
-        self.n_neighbors = n_neighbors,
+        from umap.umap_ import nearest_neighbors
+        #random_state = check_random_state(random_state)
 
-    @property
-    def neighbors(self):
-        return self._neighbors
+        knn_indices, knn_dists, forest = nearest_neighbors(
+            X,
+            random_state=0,
+            #metric_kwds=metric_kwds,
+            angular=angular,
+            verbose=verbose,
+        )
 
-    @neighbors.setter
-    def neighbors(self, n_neighbors: int):
-        if n_neighbors > len(self.data.cell_names):
-            logger.error(f'n neighbor should be less than {len(self.data.cell_names)}')
-            self._neighbors = self.neighbors
+        return knn_indices, knn_dists, forest
+
+    def find_n_neighbors(self,):
+        nbrs = NearestNeighbors(n_neighbors=self.n_neighbors+1, algorithm='ball_tree').fit(self.x)
+        dists, indices = nbrs.kneighbors(self.x)
+        nn_idx = indices[:, 1:]
+        nn_dist = dists[:, 1:]
+        return nn_idx, nn_dist
+
+    def get_igraph_from_knn(self, nn_idx, nn_dist):
+        j = nn_idx.ravel().astype(int)
+        dist = nn_dist.ravel()
+        i = np.repeat(np.arange(nn_idx.shape[0]), self.n_neighbors)
+
+        vertex = list(range(nn_dist.shape[0]))
+        edges = list(tuple(zip(i, j)))
+        G = ig.Graph()
+        G.add_vertices(vertex)
+        G.add_edges(edges)
+        G.es['weight'] = dist
+        return G
+
+    def get_igraph_from_adjacency(adjacency, directed=None):
+        """Get igraph graph from adjacency matrix."""
+        sources, targets = adjacency.nonzero()
+        weights = adjacency[sources, targets]
+        if isinstance(weights, np.matrix):
+            weights = weights.A1
+        g = ig.Graph(directed=directed)
+        g.add_vertices(adjacency.shape[0])  # this adds adjacency.shape[0] vertices
+        g.add_edges(list(zip(sources, targets)))
+        try:
+            g.es['weight'] = weights
+        except KeyError:
+            pass
+        if g.vcount() != adjacency.shape[0]:
+            logger.error(
+                f'The constructed graph has only {g.vcount()} nodes. '
+                'Your adjacency matrix contained redundant nodes.'
+            )
+        return g
+
+    def get_connectivities_umap(self, nn_idx, nn_dist):
+        from umap.umap_ import fuzzy_simplicial_set
+        n_obs = self.x.shape[0]
+        x = coo_matrix(([], ([], [])), shape=(n_obs, 1))
+        connectivities = fuzzy_simplicial_set(x, self.n_neighbors, None, None, knn_indices=nn_idx, knn_dists=nn_dist,
+                                              set_op_mix_ratio=1.0, local_connectivity=1.0)
+        if isinstance(connectivities, tuple):
+            connectivities = connectivities[0]
+        return connectivities.tocsr()
+
+    def compute_connectivities_diffmap(self, distances,density_normalize=True):
+        # init distances
+        if self.knn:
+            Dsq = distances.power(2)
+            indices, distances_sq = self.get_indices_distances_from_sparse_matrix(
+                Dsq, self.n_neighbors
+            )
         else:
-            self._neighbors = n_neighbors
+            Dsq = np.power(distances, 2)
+            indices, distances_sq = self.get_indices_distances_from_dense_matrix(
+                Dsq, self.n_neighbors
+            )
 
-    @property
-    def pca_x(self):
-        return self.pca_x
+        # exclude the first point, the 0th neighbor
+        indices = indices[:, 1:]
+        distances_sq = distances_sq[:, 1:]
 
-    @pca_x.setter
-    def pca_x(self, pca_x):
-        input_df = self._check_input_data(pca_x)
-        self.pca_x = input_df
+        # choose sigma, the heuristic here doesn't seem to make much of a difference,
+        # but is used to reproduce the figures of Haghverdi et al. (2016)
+        if self.knn:
+            # as the distances are not sorted
+            # we have decay within the n_neighbors first neighbors
+            sigmas_sq = np.median(distances_sq, axis=1)
+        else:
+            # the last item is already in its sorted position through argpartition
+            # we have decay beyond the n_neighbors neighbors
+            sigmas_sq = distances_sq[:, -1] / 4
+        sigmas = np.sqrt(sigmas_sq)
 
-    def fit(self):
-        """
-        find neighbors
-
-        :param x: input data array. [[cell_1, gene_count_1], [cell_1, gene_count_2], ..., [cell_M, gene_count_N]]
-        :return: neighbor object and neighbor cluster info
-        """
-        neighbor = Neighbors(self.pca_x, self.n_neighbors, self.n_pcs, self.metric, self.method, self.knn)
-        neighbor.check_setting()
-        self.pca_x = neighbor.choose_x()
-        use_dense_distances = (self.metric == 'euclidean' and self.pca_x.shape[0] < 8192) or not self.knn
-        dists = self.pca_x
-        if use_dense_distances:
-            dists = pairwise_distances(self.pca_x, metric=self.metric, )
-            knn_indices, knn_distances = neighbor.get_indices_distances_from_dense_matrix(dists, self.n_neighbors)
-            if self.knn:
-                dists = neighbor.get_parse_distances_numpy(
-                    knn_indices, knn_distances, self.pca_x.shape[0],
+        # compute the symmetric weight matrix
+        if not issparse(distances):
+            Num = 2 * np.multiply.outer(sigmas, sigmas)
+            Den = np.add.outer(sigmas_sq, sigmas_sq)
+            W = np.sqrt(Num / Den) * np.exp(-Dsq / Den)
+            # make the weight matrix sparse
+            if not self.knn:
+                mask = W > 1e-14
+                W[~mask] = 0
+            else:
+                # restrict number of neighbors to ~k
+                # build a symmetric mask
+                mask = np.zeros(Dsq.shape, dtype=bool)
+                for i, row in enumerate(indices):
+                    mask[i, row] = True
+                    for j in row:
+                        if i not in set(indices[j]):
+                            W[j, i] = W[i, j]
+                            mask[j, i] = True
+                # set all entries that are not nearest neighbors to zero
+                W[~mask] = 0
+        else:
+            W = (
+                Dsq.copy()
+            )  # need to copy the distance matrix here; what follows is inplace
+            for i in range(len(Dsq.indptr[:-1])):
+                row = Dsq.indices[Dsq.indptr[i] : Dsq.indptr[i + 1]]
+                num = 2 * sigmas[i] * sigmas[row]
+                den = sigmas_sq[i] + sigmas_sq[row]
+                W.data[Dsq.indptr[i] : Dsq.indptr[i + 1]] = np.sqrt(num / den) * np.exp(
+                    -Dsq.data[Dsq.indptr[i] : Dsq.indptr[i + 1]] / den
                 )
-        else:
-            if self.pca_x.shape[0] < 4096:
-                dists = pairwise_distances(self.pca_x, metric=self.metric)
-            self.metric = 'precomputed'
-            knn_indices, knn_distances, forest = neighbor.compute_neighbors_umap(dists, )
-        if not use_dense_distances or self.method in {'umap'}:
-            connectivities = neighbor.get_connectivities_umap(knn_indices, knn_distances)
-            dists = neighbor.get_parse_distances_umap(knn_indices, knn_distances, )
-        if self.method == 'gauss':
-            neighbor.compute_connectivities_diffmap(dists)
-        return neighbor, dists, connectivities
+            W = W.tolil()
+            for i, row in enumerate(indices):
+                for j in row:
+                    if i not in set(indices[j]):
+                        W[j, i] = W[i, j]
+            W = W.tocsr()
+
+        connectivities = W
+        return connectivities
+
