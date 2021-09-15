9c91f26efb24d820aee5692b010c04875cc3be4c
qiupinghust <qiupinghust@163.com>
2021-09-15 16:05:19 +0800

    update some annotation

---
+++ stereo/plots/interact_plot/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+@file: __init__.py
+@description: 
+@author: Ping Qiu
+@email: qiuping1@genomics.cn
+@last modified by: Ping Qiu
+
+change log:
+    2021/09/15  create file.
+"""
