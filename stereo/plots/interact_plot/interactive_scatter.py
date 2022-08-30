bb6828ab38de86f9425b10cd451a448449dbcef8
panjie-payne <payne6861@outlook.com>
2022-08-30 15:11:48 +0800

    temp

--- stereo/plots/interact_plot/interactive_scatter.py
+++ stereo/plots/interact_plot/interactive_scatter.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import numpy as np
 # from colorcet import palette
+from holoviews import dim
 from holoviews.selection import link_selections
 import holoviews as hv
 import hvplot.pandas
@@ -18,6 +19,7 @@ import holoviews.operation.datashader as hd
 from stereo.log_manager import logger
 import copy
 from stereo.config import StereoConfig
+from stereo.tools.boundary import ConcaveHull
 
 conf = StereoConfig()
 
@@ -72,6 +74,8 @@ class InteractiveScatter:
         )
         self.download.on_click(self._download_callback)
         self.figure = self.interact_scatter()
+        self.list_poly_selection_coors = []
+        self.list_poly_selection_exp_coors = []
 
     def generate_selected_expr_matrix(self, selected_pos, drop=False):
         if selected_pos is not None:
@@ -100,6 +104,49 @@ class InteractiveScatter:
         # sio.seek(0)
         self.download.loading = False
         # return sio
+
+    def get_selected_boundary_coors(self) -> list:
+        """
+        get selected area exp boundary coords, list contains each x,y
+        Returns:
+
+        """
+        print("processing selected {} area".format(len(self.list_poly_selection_coors)))
+        for each_polygon in self.list_poly_selection_coors:
+            if len(each_polygon):
+                selected_point = each_polygon[0]
+            else:
+                selected_point = collections.OrderedDict({'x': [], 'y': []})
+                print('selections not found, please choose a selection area')
+
+            selected_point_array = np.array([selected_point['x'], selected_point['y']])
+            points = selected_point_array.T
+            selection_expr = dim('x', spatial_select, dim('y'), geometry=points)
+            selected_pos = hv.Dataset(self.scatter_df).select(selection_expr).data.index
+            data_temp = copy.deepcopy(self.data)
+            self.selected_exp_data = data_temp.sub_by_index(cell_index=selected_pos)
+            exp_matrix_data = self.selected_exp_data.position.tolist()
+            init = ConcaveHull(exp_matrix_data, 3)
+            concave_hull = init.calculate().tolist()
+            concave_hull = [int(i) for k in concave_hull for i in k]
+            self.list_poly_selection_exp_coors.append(concave_hull)
+        return self.list_poly_selection_exp_coors
+
+    def export_high_res_area(self, origin_file_path: str, output_path: str) -> str:
+        """
+        export selected area in high resolution
+        Args:
+            origin_file_path: origin file path which you read
+            output_path: location the high res file storaged
+
+        Returns:
+            output_path
+        """
+        coors = self.get_selected_boundary_coors()
+        from gefpy.cgef_adjust_cy import CgefAdjust
+        cg = CgefAdjust()
+        cg.create_Region_Bgef(origin_file_path, output_path, coors)
+        return output_path
 
     def interact_scatter(self):
         pn.extension()
