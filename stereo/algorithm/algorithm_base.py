b01cc227599d805abbe4c674695bad6a7730d221
LyonLen <397835809@qq.com>
2022-12-05 01:26:06 +0800

    change way to get method: StPipeline actively get algorithm instead of auto-registering; add a example using algorithm_base.py.

--- stereo/algorithm/algorithm_base.py
+++ stereo/algorithm/algorithm_base.py
@@ -2,19 +2,90 @@ import re
 import sys
 import time
 import inspect
+from abc import ABCMeta
+from dataclasses import dataclass
+from typing import final
 
-from stereo.log_manager import logger
+from .algorithm_err_code import ErrorCode
+from ..log_manager import logger
+from ..core.stereo_exp_data import StereoExpData
+
+
+@dataclass
+class AlgorithmBase(metaclass=ABCMeta):
+    """
+    `AlgorithmBase` designed for auto-loading algorithms into `stereo.core.pipeline.StPipeline`.
+
+    Example Usage:
+        >>> from stereo.core import StereoExpData
+        >>> st_data = StereoExpData()
+        >>> # algorithm1 auto-loaded as a function of `StPipeline` with a snake case function name
+        >>> st_data.tl.algorithm1()
+    """
+
+    # common object variable
+    stereo_exp_data: StereoExpData
+    pipeline_res: dict = None
+
+    _steps_order_by_name = list()
+
+    @classmethod
+    def __init_subclass__(cls, **kwargs):
+        # sorted by the starting word, just like 'step1' ... 'step{N}' , before first '_' char
+        cls._steps_order_by_name = sorted(
+            [
+                (f_name, f, inspect.getfullargspec(f).args)
+                for f_name, f in cls.__dict__.items() if f_name.startswith('step')
+            ],
+            key=lambda x: x[0]
+        )
+
+    def main(self, **kwargs):
+        if not self._steps_order_by_name:
+            raise NotImplementedError(
+                f'algorithm {self.__class__.__name__} have no steps, overwrite function `main` or you can define your '
+                f'`step` function flow the doc:\n{STEP_FUNC_DESIGN_DOC}'
+            )
+        the_very_beginning_time = time.time()
+        for f_name, f, f_args_name in self._steps_order_by_name:
+            step_start_time = time.time()
+            logger.debug(f'start to run {f_name}.')
+            ret_code = f(self, **{key: value for key, value in kwargs.items() if key in f_args_name})
+            if type(ret_code) is int and ret_code != ErrorCode.Success:
+                logger.warn(f'{f_name} failed with ret_code: {ret_code}')
+                return ErrorCode.Failed
+            logger.debug(f'{f_name} end, cost: {round(time.time() - step_start_time, 4)} seconds')
+        logger.info(f'{self.__class__.__name__} end, cost: {round(time.time() - the_very_beginning_time, 4)} seconds')
+        return ErrorCode.Success
+
+    @final
+    def iter(self, *args, **kwargs):
+        for f_name, f, _ in self._steps_order_by_name:
+            ret_code = f(self, *args, **kwargs)
+            if type(ret_code) is int and ret_code != ErrorCode.Success:
+                logger.warn(f'{f_name} failed with ret_code: {ret_code}')
+                return ErrorCode.Failed
+            yield ErrorCode.Success
+
+    @final
+    def test_memory_profile(self, stream=sys.stdout, **kwargs):
+        from memory_profiler import profile
+        # these cost some `io` and `cpu` to profile memory
+        if self._steps_order_by_name:
+            for f_name, f, f_args_name in self._steps_order_by_name:
+                main_mem_check_func = profile(f, stream=stream)
+                main_mem_check_func(self, **{key: value for key, value in kwargs.items() if key in f_args_name})
+            return ErrorCode.Success
+        main_mem_check_func = profile(self.main, stream=stream)
+        main_mem_check_func(**kwargs)
+        return ErrorCode.Success
+
 
 __CAMEL_TO_SNAKE = re.compile(r"((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))")
 
 
 def camel_to_snake(value):
     return __CAMEL_TO_SNAKE.sub(r"_\1", value).lower()
-
-
-class EnumErrorCode:
-    Success, SuccessMsg = 0, 'All steps done, algorithm run perfectly! ($_$)'
-    Failed, FailedMsg = -1, 'Failed, examine the log and do not be depressed! (^_^)'
 
 
 STEP_FUNC_DESIGN_DOC = '''
@@ -32,87 +103,3 @@ STEP_FUNC_DESIGN_DOC = '''
 
     More examples are in `algorithm_example.py`.
     '''
-
-
-class AlgorithmBase(object):
-    """
-    `AlgorithmBase` designed for auto-loading algorithms into `stereo.core.pipeline.StPipeline`.
-
-    Example Usage:
-        >>> # first, register the algorithms you want to use
-        >>> from stereo.algorithm.algorithm_example import Algorithm1
-        >>> # Output:
-        >>> #     algorithm name: Algorithm1 is register to class AlgorithmBase
-        >>> from stereo.core import StereoExpData
-        >>> st_data = StereoExpData()
-        >>> # algorithm1 auto-loaded as a function of `StPipeline` with a snake case function name
-        >>> st_data.tl.algorithm1()
-    """
-
-    __SUB_CLASSES = dict()
-    __STEPS_ORDER_BY_NAME = list()
-
-    @staticmethod
-    def __register_subclass_to_base(sub_cls):
-        # convert camel case as snake case, will be as function name in pipeline
-        camel_sub_cls_name = camel_to_snake(sub_cls.__name__)
-        assert camel_sub_cls_name not in AlgorithmBase.__SUB_CLASSES, f'{camel_sub_cls_name} already registered'
-        AlgorithmBase.__SUB_CLASSES[camel_sub_cls_name] = sub_cls
-        logger.info(f'algorithm name: {sub_cls.__name__} is registered to `AlgorithmBase`')
-
-    @staticmethod
-    def _get_all_subclass():
-        return AlgorithmBase.__SUB_CLASSES.items()
-
-    @classmethod
-    def __init_subclass__(cls, **kwargs):
-        cls.__SUB_CLASSES = dict()
-        # sorted by the starting word, just like 'step1' ... 'step{N}' , before first '_' char
-        cls.__STEPS_ORDER_BY_NAME = sorted(
-            [(f_name, f, inspect.getfullargspec(f).args) for f_name, f in cls.__dict__.items() if
-             f_name.startswith('step')],
-            key=lambda x: x[0].split('_')[0]
-        )
-        AlgorithmBase.__register_subclass_to_base(cls)
-
-    def __init__(self, stereo_exp_data):
-        # all algorithm are design for `StereoExpData` analysis
-        self.stereo_exp_data = stereo_exp_data
-
-    def main(self, **kwargs):
-        if not self.__STEPS_ORDER_BY_NAME:
-            raise NotImplementedError(
-                f'algorithm {self.__class__.__name__} have no steps, overwrite function `main` or you can define your '
-                f'`step` function flow the doc:\n{STEP_FUNC_DESIGN_DOC}'
-            )
-        the_very_beginning_time = time.time()
-        for f_name, f, f_args_name in self.__STEPS_ORDER_BY_NAME:
-            step_start_time = time.time()
-            logger.debug(f'start to run {f_name}.')
-            ret_code = f(self, **{key: value for key, value in kwargs.items() if key in f_args_name})
-            if type(ret_code) is int and ret_code != EnumErrorCode.Success:
-                logger.warn(f'{f_name} failed with ret_code: {ret_code}')
-                return EnumErrorCode.Failed
-            logger.debug(f'{f_name} end, cost: {round(time.time() - step_start_time, 4)} seconds')
-        logger.info(f'{self.__class__.__name__} end, cost: {round(time.time() - the_very_beginning_time, 4)} seconds')
-        return EnumErrorCode.Success
-
-    def iter(self, *args, **kwargs):
-        for f_name, f, _ in self.__STEPS_ORDER_BY_NAME:
-            ret_code = f(self, *args, **kwargs)
-            if type(ret_code) is int and ret_code != EnumErrorCode.Success:
-                logger.warn(f'{f_name} failed with ret_code: {ret_code}')
-                return EnumErrorCode.Failed
-            yield EnumErrorCode.Success
-
-    def test_memory_profile_step_by_step(self, stream=sys.stdout, **kwargs):
-        from memory_profiler import profile
-        # these cost a little bit `io` and `cpu` to profile memory usage
-        if self.__STEPS_ORDER_BY_NAME:
-            for f_name, f, f_args_name in self.__STEPS_ORDER_BY_NAME:
-                main_mem_check_func = profile(f, stream=stream)
-                main_mem_check_func(self, **{key: value for key, value in kwargs.items() if key in f_args_name})
-            return EnumErrorCode.Success
-        main_mem_check_func = profile(self.main, stream=stream)
-        main_mem_check_func(**kwargs)
-        return EnumErrorCode.Success
