784177cdca358c5cc58cfccca0a7eb6733d03c9f
LyonLen <397835809@qq.com>
2022-12-29 14:10:20 +0800

    a child class stereo_exp_data based on AnnData, this is a beta design

--- stereo/core/cell.py
+++ stereo/core/cell.py
@@ -15,10 +15,12 @@ from typing import Optional, Union
 
 import numpy as np
 import pandas as pd
+from anndata import AnnData
 
 
 class Cell(object):
-    def __init__(self, cell_name: Optional[np.ndarray] = None, cell_border: Optional[np.ndarray] = None, batch: Optional[Union[np.ndarray, list, int, str]] = None):
+    def __init__(self, cell_name: Optional[np.ndarray] = None, cell_border: Optional[np.ndarray] = None,
+                 batch: Optional[Union[np.ndarray, list, int, str]] = None):
         self._cell_name = cell_name
         self._cell_border = cell_border
         self._batch = self._set_batch(batch) if batch is not None else None
@@ -46,21 +48,21 @@ class Cell(object):
         if not isinstance(name, np.ndarray):
             raise TypeError('cell name must be a np.ndarray object.')
         self._cell_name = name
-    
+
     @property
     def cell_border(self):
         return self._cell_border
-    
+
     @cell_border.setter
     def cell_boder(self, cell_border: np.ndarray):
         if not isinstance(cell_border, np.ndarray):
             raise TypeError('cell border must be a np.ndarray object.')
         self._cell_border = cell_border
-    
+
     @property
     def batch(self):
         return self._batch
-    
+
     @batch.setter
     def batch(self, batch: Union[np.ndarray, list, int]):
         self._batch = self._set_batch(batch)
@@ -68,10 +70,12 @@ class Cell(object):
     def _set_batch(self, batch: Union[np.ndarray, list, int]):
         if batch is None:
             return None
-            
-        if not isinstance(batch, np.ndarray) and not isinstance(batch, list) and not isinstance(batch, int) and not isinstance(batch, str):
+
+        if not isinstance(batch, np.ndarray) and not isinstance(batch, list) and not isinstance(batch,
+                                                                                                int) and not isinstance(
+            batch, str):
             raise TypeError('batch must be np.ndarray or list or int or str')
-        
+
         if isinstance(batch, int):
             batch = str(batch)
         if isinstance(batch, str):
@@ -129,3 +133,60 @@ class Cell(object):
             attributes['batch'] = self._batch
         df = pd.DataFrame(attributes, index=self.cell_name)
         return df
+
+
+class AnnBasedCell(Cell):
+
+    def __init__(self, based_ann_data: AnnData, cell_name: Optional[np.ndarray] = None,
+                 cell_border: Optional[np.ndarray] = None,
+                 batch: Optional[Union[np.ndarray, list, int, str]] = None):
+        self.__based_ann_data = based_ann_data
+        super(AnnBasedCell, self).__init__(cell_name, cell_border, batch)
+
+    @property
+    def cell_name(self) -> np.ndarray:
+        """
+        get the name of cell.
+
+        :return: cell name
+        """
+        return self.__based_ann_data.obs_names.values.astype(str)
+
+    @cell_name.setter
+    def cell_name(self, name: np.ndarray):
+        """
+        set the name of cell.
+
+        :param name: a numpy array of names.
+        :return:
+        """
+        if not isinstance(name, np.ndarray):
+            raise TypeError('cell name must be a np.ndarray object.')
+        self.__based_ann_data._inplace_subset_obs(name)
+
+    @property
+    def total_counts(self):
+        return self.__based_ann_data.obs['total_counts']
+
+    @total_counts.setter
+    def total_counts(self, new_total_counts):
+        if new_total_counts is not None:
+            self.__based_ann_data.obs['total_counts'] = new_total_counts
+
+    @property
+    def pct_counts_mt(self):
+        return self.__based_ann_data.obs['pct_counts_mt']
+
+    @pct_counts_mt.setter
+    def pct_counts_mt(self, new_pct_counts_mt):
+        if new_pct_counts_mt is not None:
+            self.__based_ann_data.obs['pct_counts_mt'] = new_pct_counts_mt
+
+    @property
+    def n_genes_by_counts(self):
+        return self.__based_ann_data.obs['n_genes_by_counts']
+
+    @n_genes_by_counts.setter
+    def n_genes_by_counts(self, new_n_genes_by_counts):
+        if new_n_genes_by_counts is not None:
+            self.__based_ann_data.obs['n_genes_by_counts'] = new_n_genes_by_counts
