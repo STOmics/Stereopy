c58dd0dfca5147b9424f6299555a02b529417805
tanliwei <tanliwei@genomics.cn>
2023-01-06 10:55:07 +0800

    fix bug

--- stereo/utils/pipeline_utils.py
+++ stereo/utils/pipeline_utils.py
@@ -27,7 +27,7 @@ def cell_cluster_to_gene_exp_cluster(tl: StPipeline, cluster_res_key: str=None):
     tmp = []
     for _, cell_index in group_index.iterrows():
         cell_index = cell_index.to_numpy()[0]
-        sub_exp_matrix = tl.raw.exp_matrix[cell_index].toarray() if tl.raw.issparse() else data.tl.raw.exp_matrix[cell_index]
+        sub_exp_matrix = tl.raw.exp_matrix[cell_index].toarray() if tl.raw.issparse() else tl.raw.exp_matrix[cell_index]
         tmp.append(sub_exp_matrix.sum(axis=0))
     cluster_exp_matrix = np.vstack(tmp)
     return pd.DataFrame(cluster_exp_matrix, columns=tl.data.gene_names, index=group_index.index).T 
