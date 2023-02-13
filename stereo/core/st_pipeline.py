f803936312a042a8421acf45d9f9bf3482d69c8d
LyonLen <397835809@qq.com>
2023-02-13 13:37:36 +0800

    fix bugs in AnndataBasedStereoExpData

--- stereo/core/st_pipeline.py
+++ stereo/core/st_pipeline.py
@@ -1032,6 +1032,9 @@ class AnnBasedResult(dict):
         obsm_obj = self.__based_ann_data.obsm.get(f'X_{item}', None)
         if obsm_obj is not None:
             return True
+        obsm_obj = self.__based_ann_data.obsm.get(f'{item}', None)
+        if obsm_obj is not None:
+            return True
         obs_obj = self.__based_ann_data.obs.get(item, None)
         if obs_obj is not None:
             return True
@@ -1056,6 +1059,9 @@ class AnnBasedResult(dict):
             # TODO ignore `mean_bin`, really need?
             return self.__based_ann_data.var.loc[:, ["means", "dispersions", "dispersions_norm", "highly_variable"]]
         obsm_obj = self.__based_ann_data.obsm.get(f'X_{name}', None)
+        if obsm_obj is not None:
+            return pd.DataFrame(obsm_obj)
+        obsm_obj = self.__based_ann_data.obsm.get(f'{name}', None)
         if obsm_obj is not None:
             return pd.DataFrame(obsm_obj)
         obs_obj = self.__based_ann_data.obs.get(name, None)
