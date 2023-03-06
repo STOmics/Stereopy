5af9073ee5b4a01794a018ec0c3b12c9b4ffbab2
LyonLen <397835809@qq.com>
2023-03-06 10:15:31 +0800

    AnnDataBasedStereoExpData: change the super class init code, less than before

--- stereo/core/st_pipeline.py
+++ stereo/core/st_pipeline.py
@@ -1089,7 +1089,7 @@ class AnnBasedResult(dict):
     }
 
     def __init__(self, based_ann_data: AnnData):
-        super(dict, self).__init__()
+        super().__init__()
         self.__based_ann_data = based_ann_data
 
     def __contains__(self, item):
@@ -1256,7 +1256,7 @@ class AnnBasedResult(dict):
 class AnnBasedStPipeline(StPipeline):
 
     def __init__(self, based_ann_data: AnnData, data):
-        super(AnnBasedStPipeline, self).__init__(data)
+        super().__init__(data)
         self.__based_ann_data = based_ann_data
         self.result = AnnBasedResult(based_ann_data)
 
