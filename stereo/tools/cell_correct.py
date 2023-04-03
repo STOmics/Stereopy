b8624b109839da33220cfeab480a8e1a509d62d5
tanliwei <tanliwei@genomics.cn>
2023-04-03 09:37:25 +0800

    update for cell correction.

--- stereo/tools/cell_correct.py
+++ stereo/tools/cell_correct.py
@@ -15,7 +15,7 @@ from ..algorithm import cell_correction_fast_by_mask
 from ..algorithm.draw_contours import DrawContours
 from ..io import read_gem, read_gef
 from ..log_manager import logger
-from gefpy import cgef_writer_cy, bgef_writer_cy, cgef_adjust_cy
+from gefpy import cgef_writer_cy, bgef_writer_cy, cgef_adjust_cy, gef_to_gem_cy
 from ..utils.time_consume import TimeConsume, log_consumed_time
 from .gem_filter import gem_filter
 
@@ -59,6 +59,7 @@ class CellCorrect(object):
         self.cad = cgef_adjust_cy.CgefAdjust()
         self.gene_names = None
         self.check_input()
+        self.sn = self.get_sn()
 
     def check_input(self):
         if self.bgef_path is None and self.gem_path is None:
@@ -73,14 +74,20 @@ class CellCorrect(object):
         if self.bgef_path is None:
             self.bgef_path = self.generate_bgef()
     
+    def get_sn(self):
+        if self.bgef_path is not None:
+            file_name = os.path.basename(self.bgef_path)
+        else:
+            file_name = os.path.basename(self.gem_path)
+        return file_name.split('.')[0]
+
     def get_file_name(self, ext=None):
         ext = ext.lstrip('.') if ext is not None else ""
         if self.bgef_path is not None:
             file_name = os.path.basename(self.bgef_path)
-            file_prefix = os.path.splitext(file_name)[0]
         else:
             file_name = os.path.basename(self.gem_path)
-            file_prefix = os.path.splitext(file_name)[0]
+        file_prefix = file_name.split('.')[0]
         if ext == "":
             return file_prefix
         else:
@@ -96,7 +103,7 @@ class CellCorrect(object):
         bgef_writer_cy.generate_bgef(self.gem_path, bgef_path, n_thread=threads, bin_sizes=[1])
         return bgef_path
     
-    def generate_cgef(self, mask_path, ext_in_ext):
+    def generate_cgef_with_mask(self, mask_path, ext_in_ext):
         file_name = self.get_file_name(f'{ext_in_ext}.cellbin.gef')
         cgef_path = os.path.join(self.out_dir, file_name)
         logger.info(f"start to generate cellbin gef ({cgef_path})")
@@ -125,7 +132,7 @@ class CellCorrect(object):
     @log_consumed_time
     def generate_raw_data(self, sample_n=-1):
         if self.raw_cgef_path is None:
-            self.raw_cgef_path = self.generate_cgef(self.mask_path, 'raw')
+            self.raw_cgef_path = self.generate_cgef_with_mask(self.mask_path, 'raw')
         
         logger.info("start to generate raw data")
         genes, raw_data = self.get_data_from_bgef_and_cgef(self.bgef_path, self.raw_cgef_path, sample_n=sample_n)
@@ -147,12 +154,15 @@ class CellCorrect(object):
         cell = np.array(cell_data, dtype=cell_type)
         dnb = np.array(dnb_data, dtype=dnb_type)
         file_name = self.get_file_name('adjusted.cellbin.gef')
-        adjust_cgef_file = os.path.join(self.out_dir, file_name)
-        if os.path.exists(adjust_cgef_file):
-            os.remove(adjust_cgef_file)
-        self.cad.write_cgef_adjustdata(adjust_cgef_file, cell, dnb, outline_path)
-        logger.info(f"generate adjusted cellbin gef finished ({adjust_cgef_file})")
-        return adjust_cgef_file
+        cgef_file_adjusted = os.path.join(self.out_dir, file_name)
+        if os.path.exists(cgef_file_adjusted):
+            os.remove(cgef_file_adjusted)
+        if outline_path is not None:
+            self.cad.write_cgef_adjustdata(cgef_file_adjusted, cell, dnb, outline_path)
+        else:
+            self.cad.write_cgef_adjustdata(cgef_file_adjusted, cell, dnb)
+        logger.info(f"generate adjusted cellbin gef finished ({cgef_file_adjusted})")
+        return cgef_file_adjusted
     
     @log_consumed_time
     def generate_adjusted_gem(self, adjusted_data: pd.DataFrame):
@@ -164,11 +174,26 @@ class CellCorrect(object):
         adjusted_data.to_csv(gem_file_adjusted, sep="\t", index=False, columns=columns)
         logger.info(f"generate adjusted gem finished ({gem_file_adjusted})")
         return gem_file_adjusted
+    
+    @log_consumed_time
+    def cgef_to_gem(self, cgef_path):
+        file_name = self.get_file_name("adjusted.gem")
+        gem_file_adjusted = os.path.join(self.out_dir, file_name)
+        obj = gef_to_gem_cy.gefToGem(gem_file_adjusted, self.sn)
+        obj.cgef2gem(cgef_path, self.bgef_path)
+        return gem_file_adjusted
+
+    @log_consumed_time
+    def bgef_to_gem(self, mask_path):
+        file_name = self.get_file_name("adjusted.gem")
+        gem_file_adjusted = os.path.join(self.out_dir, file_name)
+        obj = gef_to_gem_cy.gefToGem(gem_file_adjusted, self.sn)
+        obj.bgef2cgem(mask_path, self.bgef_path)
 
     def __set_processes_count(self, process_count, fast):
         if process_count is not None:
             if not isinstance(process_count, int):
-                raise ValueError("the prameter 'process_count' must be set to int.")
+                raise TypeError("the type of prameter 'process_count' must be int.")
             
         if isinstance(fast, bool) and (not fast):
             if process_count is None or process_count == 0:
@@ -177,7 +202,7 @@ class CellCorrect(object):
                 process_count = cpu_count()
         else:
             if isinstance(fast, bool) and fast:
-                fast = 'v1'
+                fast = 'v2'
             if fast == 'v1':
                 process_count = 1
             elif fast == 'v2':
@@ -185,45 +210,75 @@ class CellCorrect(object):
                     process_count = 1
                 elif process_count < 0 or process_count > cpu_count():
                     process_count = cpu_count()
+            else:
+                process_count = 1 if process_count is None else process_count
         return process_count
+    
+    def __set_contours_fitting(self, contours_fitting, fast):
+        if contours_fitting is not None:
+            if not isinstance(contours_fitting, bool):
+                raise TypeError("the type of parameter 'contours_fitting' must be bool.")
+
+        if isinstance(fast, bool) and (not fast):
+            if contours_fitting is None:
+                contours_fitting = True
+        else:
+            if isinstance(fast, bool) and fast:
+                fast = 'v2'
+            if fast == 'v1':
+                if contours_fitting is None:
+                    contours_fitting = True
+            elif fast == 'v2':
+                if contours_fitting is None:
+                    contours_fitting = False
+            else:
+                contours_fitting = True if contours_fitting is None else contours_fitting
+        return contours_fitting
+
 
     @log_consumed_time
-    def correcting(self, threshold=20, process_count=None, only_save_result=False, sample_n=-1, fast=True, **kwags):
-        if isinstance(fast, str):
+    def correcting(self, threshold=20, process_count=None, only_save_result=False, sample_n=-1, fast=True, **kwargs):
+        if isinstance(fast, bool) and fast:
+            fast = 'v2'
+        elif isinstance(fast, str):
             fast = fast.lower()
-            if fast == 'v1':
-                fast = True
         process_count = self.__set_processes_count(process_count, fast)
-        logger.info(f"Cell correction runs on {process_count} jobs.")
-        if isinstance(fast, bool):
+        if fast is False or fast == 'v1':
             genes, raw_data = self.generate_raw_data(sample_n)
             if self.tissue_mask_path is not None:
                 raw_data = gem_filter(self.tissue_mask_path, raw_data)
-            if not fast:
-                correction = CellCorrection(self.mask_path, raw_data, threshold, process_count, err_log_dir=self.out_dir)
-                adjusted_data = correction.cell_correct()
-            else:
-                adjusted_data = cell_correction_fast.cell_correct(raw_data, self.mask_path)
+        if fast is False:
+            correction = CellCorrection(self.mask_path, raw_data, threshold, process_count, err_log_dir=self.out_dir)
+            adjusted_data = correction.cell_correct()
+        elif fast == 'v1':
+            adjusted_data = cell_correction_fast.cell_correct(raw_data, self.mask_path)
+        elif fast == 'v2':
+            new_kwargs = {}
+            if 'n_split_data_jobs' in kwargs:
+                new_kwargs['n_split_data_jobs'] = kwargs['n_split_data_jobs']
+                del kwargs['n_split_data_jobs']
+            if 'distance' in kwargs:
+                new_kwargs['distance'] = kwargs['distance']
+                del kwargs['distance']
+            new_mask_path = cell_correction_fast_by_mask.main(self.mask_path, n_jobs=process_count, out_path=self.out_dir, **new_kwargs)
+            cgef_file_adjusted = self.generate_cgef_with_mask(new_mask_path, 'adjusted')
+            gem_file_adjusted = self.cgef_to_gem(cgef_file_adjusted)
+            # gem_file_adjusted = self.bgef_to_gem(new_mask_path)
+            # genes, adjusted_data_dirty = self.get_data_from_bgef_and_cgef(self.bgef_path, new_cgef_path)
+            # logger.info(f"adjusted_data_dirty.shape = {adjusted_data_dirty.shape}")
+            # adjusted_data = adjusted_data_dirty[adjusted_data_dirty['label'] != 0]
+            # logger.info(f"adjusted_data.shape = {adjusted_data.shape}")
         else:
-            if fast == 'v2':
-                new_kwags = {}
-                if 'n_split_data_jobs' in kwags:
-                    new_kwags['n_split_data_jobs'] = kwags['n_split_data_jobs']
-                    del kwags['n_split_data_jobs']
-                if 'distance' in kwags:
-                    new_kwags['distance'] = kwags['distance']
-                    del kwags['distance']
-                new_mask_path = cell_correction_fast_by_mask.main(self.mask_path, n_jobs=process_count, out_path=self.out_dir, **new_kwags)
-                new_cgef_path = self.generate_cgef(new_mask_path, 'dirty')
-                genes, adjusted_data_dirty = self.get_data_from_bgef_and_cgef(self.bgef_path, new_cgef_path)
-                adjusted_data = adjusted_data_dirty[adjusted_data_dirty['label'] != 0]
-            else:
-                raise ValueError(f"Unexpected value({fast}) for parameter fast, available values include [True, False, 'v1', 'v2'].")
+            raise ValueError(f"Unexpected value({fast}) for parameter fast, available values include [False, 'v1', 'v2'].")
 
-        gem_file_adjusted = self.generate_adjusted_gem(adjusted_data)
-        dc = DrawContours(adjusted_data, self.out_dir)
-        outline_path = dc.get_contours()
-        cgef_file_adjusted = self.generate_adjusted_cgef(adjusted_data, outline_path)
+        if fast == 'v2':
+            pass
+        else:
+            gem_file_adjusted = self.generate_adjusted_gem(adjusted_data)
+            dc = DrawContours(adjusted_data, self.out_dir)
+            outline_path = dc.get_contours()
+            cgef_file_adjusted = self.generate_adjusted_cgef(adjusted_data, outline_path)
+
         if not only_save_result:
             return read_gef(cgef_file_adjusted, bin_type='cell_bins')
         else:
@@ -245,7 +300,7 @@ def cell_correct(out_dir: str,
                 only_save_result: bool=False,
                 fast: Union[bool, str]='v2',
                 tissue_mask_path: str=None,
-                **kwags
+                **kwargs
 ):
 
     """
@@ -253,6 +308,8 @@ def cell_correct(out_dir: str,
         * GEM and mask
         * GEM and ssDNA image
         * BGEF and mask
+        * BGEF and ssDNA image
+        * GEM and raw CGEF (not have been corrected)
         * BGEF and raw CGEF (not have been corrected)
 
     :param out_dir: the path to save intermediate result, like mask (if generate from ssDNA image), 
@@ -260,21 +317,21 @@ def cell_correct(out_dir: str,
     :param threshold: threshold size, default to 20.
     :param gem_path: the path to GEM file.
     :param bgef_path: the path to BGEF file.
-    :param raw_cgef_path: the path to CGEF file in where data has not been corrected.
+    :param raw_cgef_path: the path to CGEF file which not has been corrected.
     :param mask_path: the path to mask file.
     :param image_path: the path to ssDNA image file.
     :param model_path: the path to model file.
     :param mask_save: whether to save mask file after correction, generated from ssDNA image.
     :param model_type: the type of model to generate mask, whcih only could be set to deep learning model and deep cell model.
-	:param gpu: specify gpu id to predict when generate mask, if `'-1'`, use cpu for prediction.
+	:param gpu: specify gpu id to predict when generate mask, if `'-1'`, cpu is used.
     :param process_count: the count of the process will be started when correct cells, defaults to None
                 by default, it will be set to 10 when `fast` is set to False and will be set to 1 when `fast` is set to True, v1 or v2.
                 if it is set to -1, all of the cores will be used.
 	:param only_save_result: if `True`, only save result to disk; if `False`, return an StereoExpData object.
-    :param fast: specify the version of algorithm, available values include [False, True, v1, v2], defaults to v2.
+    :param fast: specify the version of algorithm, available values include [False, v1, v2], defaults to v2.
                     False: the oldest and slowest version, it will uses multiprocessing if set `process_count` to more than 1.
-                    True or v1: the first fast version, it olny uses single process and single threading.
-                    v2: default algorithm, the latest fast version, faster than v1, it will uses multithreading if set `process_count` to more than 1.
+                    v1: the first fast version, it olny uses single process and single threading.
+                    v2: default and recommended algorithm, the latest fast version, faster and more accurate than v1, it will uses multithreading if set `process_count` to more than 1.
     :param tissue_mask_path: the path of tissue mask, default to None.
                     if it is set, the data will be filterd by tissue_mask, unavailable if set the `fast` to v2.
     :param deep_cro_size: deep crop size.
@@ -286,12 +343,12 @@ def cell_correct(out_dir: str,
     if mask_path is None and image_path is not None:
         from .cell_segment import CellSegment
         do_mask_generating = True
-        deep_cro_size = kwags.get('deep_cro_size', 20000)
-        overlap = kwags.get('overlap', 100)
-        if 'deep_cro_size' in kwags:
-            del kwags['deep_cro_size']
-        if 'overlap' in kwags:
-            del kwags['overlap']
+        deep_cro_size = kwargs.get('deep_cro_size', 20000)
+        overlap = kwargs.get('overlap', 100)
+        if 'deep_cro_size' in kwargs:
+            del kwargs['deep_cro_size']
+        if 'overlap' in kwargs:
+            del kwargs['overlap']
         cell_segment = CellSegment(image_path, gpu, out_dir)
         logger.info(f"there is no cell mask file, generate it by model {model_path}")
         cell_segment.generate_mask(model_path, model_type, deep_cro_size, overlap)
@@ -299,7 +356,7 @@ def cell_correct(out_dir: str,
         logger.info(f"the generated cell mask file {mask_path}")
         
     cc = CellCorrect(gem_path=gem_path, bgef_path=bgef_path, raw_cgef_path=raw_cgef_path, mask_path=mask_path, tissue_mask_path=tissue_mask_path, out_dir=out_dir)
-    adjusted_data = cc.correcting(threshold=threshold, process_count=process_count, only_save_result=only_save_result, fast=fast, **kwags)
+    adjusted_data = cc.correcting(threshold=threshold, process_count=process_count, only_save_result=only_save_result, fast=fast, **kwargs)
     if do_mask_generating and not mask_save:
         cell_segment.remove_all_mask_files()
     return adjusted_data
