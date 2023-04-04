72c0db92dc082d08b9b9ac3ef32e6abfa650d24a
xujunhao <xujunhao@genomics.cn>
2023-04-04 16:22:30 +0800

    update setup file

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ if sys.version_info < (3, 8):
 
 setup(
     name='stereopy',
-    version='0.10.0',
+    version='0.11.0',
     setup_requires=['setuptools_scm', 'numpy==1.21.4', 'panel', 'pytest', 'quilt3', 'scipy', 'phenograph'],
     description='Spatial transcriptomic analysis in python.',
     long_description=Path('README.md').read_text('utf-8'),
     long_description_content_type="text/markdown",
     url='https://github.com/BGIResearch/stereopy',
     author='BGIResearch',
-    author_email='qiuping1@genomics.cn',
+    author_email='xujunhao@genomics.cn',
     python_requires='>=3.8,<3.9',
     install_requires=[
         l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
