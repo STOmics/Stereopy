b45b6d176032a0ef3cfb6e1c4bd4f274eee7d254
tanliwei <tanliwei@genomics.cn>
2023-06-20 18:30:32 +0800

    update documents.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ todo_include_todos = False
 api_dir = HERE / 'api'  # function_images
 # print(api_dir)
 # The master toctree document.
-language = None
+language = 'en'
 
 #If you want to include a notebook without outputs and yet don’t want nbsphinx to execute it for you, you can explicitly disable this feature.
 nbsphinx_execute = 'never'
