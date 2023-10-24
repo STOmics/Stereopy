c427c09c7886904d9adc8ff6d4861b7f20d05185
tanliwei <tanliwei@genomics.cn>
2023-10-24 17:35:48 +0800

    change the name of spatial_alignment to paste.

--- stereo/algorithm/paste/__init__.py
+++ stereo/algorithm/paste/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa
-from .spatial_alignment import SpatialAlignment
+from .paste import Paste
 from .methods import pairwise_align, center_align
--- stereo/algorithm/paste/paste.py
+++ stereo/algorithm/paste/paste.py
@@ -6,13 +6,14 @@ from typing import (
 )
 
 from stereo.algorithm.ms_algorithm_base import MSDataAlgorithmBase
+from stereo.log_manager import logger
 from .helper import stack_slices_center
 from .helper import stack_slices_pairwise
 from .methods import center_align
 from .methods import pairwise_align
 
 
-class SpatialAlignment(MSDataAlgorithmBase):
+class Paste(MSDataAlgorithmBase):
     def main(
             self,
             method: str = 'pairwise',
@@ -33,10 +34,13 @@ class SpatialAlignment(MSDataAlgorithmBase):
         if method not in ('pairwise', 'center'):
             raise ValueError(f'Error method({method}), it must be one of pairwise and center')
 
+        logger.info(f'Using method {method}')
         if method == 'pairwise':
             if slices is None:
+                slice_names = self.ms_data.names
                 slices = self.ms_data.data_list
             else:
+                slice_names = [s if isinstance(s, str) else self.ms_data.names[s] for s in slices]
                 slices = [self.ms_data[s] for s in slices]
             assert len(slices) >= 2, "You should have at least 2 slices on 'pairwise_align' method"
 
@@ -44,16 +48,20 @@ class SpatialAlignment(MSDataAlgorithmBase):
             pi_pairs = []
             for i in range(scount - 1):
                 slice_a, slice_b = slices[i], slices[i + 1]
+                logger.info(f'Processing slice {slice_names[i]} and {slice_names[i + 1]}')
                 pi_pairs.append(pairwise_align(slice_a, slice_b, *args, **kwargs))
             stack_slices_pairwise(slices, pi_pairs)
         else:
             if slices is None:
+                slice_names = self.ms_data.names
                 slices = self.ms_data.data_list
             else:
+                slice_names = [s if isinstance(s, str) else self.ms_data.names[s] for s in slices]
                 slices = [self.ms_data[s] for s in slices]
             assert len(slices) >= 2, "You should have at least 2 slices on 'center_align' method"
             if initial_slice is None:
                 initial_slice = deepcopy(slices[0])
+                initial_slice_name = slice_names[0]
             else:
                 initial_slice = deepcopy(self.ms_data[initial_slice])
             center_slice, pis = center_align(initial_slice, slices, *args, **kwargs)
