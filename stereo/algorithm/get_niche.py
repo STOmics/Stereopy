29f3015a2d633e5f7cda3276458b489ca573abc7
tanliwei <liweitan@aliyun.com>
2024-06-12 14:30:38 +0800

    update for GetNiche

--- stereo/algorithm/get_niche.py
+++ stereo/algorithm/get_niche.py
@@ -26,17 +26,14 @@ class GetNiche(AlgorithmBase):
         :param cluster_2: the other cell cluster in the interaction.
         :param coord_key: the key which specifies the coordiate of cells.
         :param cluster_res_key: the key which specifies the clustering result in data.tl.result.
-        :param inplace: whether to inplace the previous express matrix or get a new StereoExpData object with the new express matrix, default by False. # noqa
+        :param inplace: whether to replace the previous express matrix or get a new StereoExpData object with the new express matrix, default by False. # noqa
         """
         assert cluster_1 != cluster_2, "cluster_1 can not equal to cluster_2."
 
         data_full = self.stereo_exp_data
-        # adata_full = self.stereo_exp_data._ann_data
         cluster = self.pipeline_res[cluster_res_key]
         data_1, _ = filter_by_clusters(data_full, cluster_res=cluster, groups=cluster_1, inplace=False)
         data_2, _ = filter_by_clusters(data_full, cluster_res=cluster, groups=cluster_2, inplace=False)
-        # adata_1 = adata_full[adata_full.obs['celltype'] == cluster_1, :]
-        # adata_2 = adata_full[adata_full.obs['celltype'] == cluster_2, :]
 
         coord_1 = data_1.position
         coord_2 = data_2.position
@@ -50,31 +47,19 @@ class GetNiche(AlgorithmBase):
         result_target_sender = dist_df.where(dist_df < niche_distance, other=np.nan)
         result_target_sender = result_target_sender.dropna(how='all', axis=1).dropna(how='all', axis=0)
 
-        # adata_result = adata_full[(list(result_target_sender.index) + list(result_target_sender.columns)), :]
         cell_list = list(result_target_sender.index) + list(result_target_sender.columns)
         data_result = filter_cells(data_full, cell_list=cell_list, inplace=inplace)
-        if not inplace:
-            data_result.tl.result.set_item_callback = None
 
-        for res_key in data_result.tl.key_record['pca']:
+        for res_key in (data_result.tl.key_record['pca'] + data_result.tl.key_record['umap']):
             if data_result.tl.result[res_key].shape[0] == data_result.shape[0]:
                 continue
-            data_result.tl.result[res_key] = data_result.tl.result[res_key][
-                np.isin(data_full.cell_names, cell_list)].copy()
-        if cluster_res_key in data_result.cells:
-            data_result.cells[cluster_res_key] = pd.Series(
-                data_result.cells[cluster_res_key].to_numpy(),
-                index=data_result.cell_names,
-                dtype='category'
-            )
-        else:
-            data_result.tl.result[cluster_res_key]['group'] = pd.Series(
-                data_result.tl.result[cluster_res_key]['group'].to_numpy(),
-                index=data_result.cell_names,
-                dtype='category'
-            )
+            flag = np.isin(data_full.cell_names, cell_list)
+            res_filtered = data_result.tl.result[res_key][flag]
+            res_filtered.reset_index(drop=True, inplace=True)
+            data_result.tl.result[res_key] = res_filtered
+        
+        for column in data_result.cells.obs.columns:
+            if data_result.cells.obs[column].dtype.name == 'category':
+                data_result.cells.obs[column] = data_result.cells.obs[column].cat.remove_unused_categories()
 
-        if not inplace:
-            data_result.tl.result.contain_method = None
-            data_result.tl.result.get_item_method = None
         return data_result
