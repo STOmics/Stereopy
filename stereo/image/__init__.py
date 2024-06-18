31ebc4f7fa6c388e54208ac54c0f0f36b019d87e
tanliwei <liweitan@aliyun.com>
2024-06-18 14:04:21 +0800

    update some comments.

--- stereo/image/__init__.py
+++ stereo/image/__init__.py
@@ -9,15 +9,16 @@ try:
     from . import tissue_cut
     from .segmentation_deepcell.segment import cell_seg_deepcell
 except ImportError as e:
-    errmsg = """function `merge_pyramid`, `create_pyramid`, `cell_seg`, `cell_seg_v3`, `tissue_cut`, 
-`cell_seg_deepcell` is not import at `stereo.image` module.
-************************************************
-* Some necessary modules may not be installed. *
-* Please install them by:                      *
-*   pip install tensorflow==2.7.0              *
-*   pip install torch==1.10.0                  *
-*   pip install torchvision==0.11.1            *
-*   pip install albumentations==0.4.6          *
-************************************************
-    """
+#     errmsg = """function `merge_pyramid`, `create_pyramid`, `cell_seg`, `cell_seg_v3`, `tissue_cut`, 
+# `cell_seg_deepcell` is not import at `stereo.image` module.
+# ************************************************
+# * Some necessary modules may not be installed. *
+# * Please install them by:                      *
+# *   pip install tensorflow==2.7.0              *
+# *   pip install torch==1.10.0                  *
+# *   pip install torchvision==0.11.1            *
+# *   pip install albumentations==0.4.6          *
+# ************************************************
+#     """
+    errmsg = "Some necessary modules may not be installed. More information please refer to the documentation."
     raise ImportError(errmsg)
